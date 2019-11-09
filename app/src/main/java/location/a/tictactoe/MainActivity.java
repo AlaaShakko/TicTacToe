740fa89f36dde1119ea108d1498c4e338577646c
U150709049 <lulu.shakko@gmail.com>
2019-11-09 15:40:15 +0300

    TicTacToe

--- app/src/main/java/location/a/tictactoe/MainActivity.java
+++ app/src/main/java/location/a/tictactoe/MainActivity.java
@@ -1,5 +1,7 @@
 package location.a.tictactoe;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 
 import android.os.Bundle;
@@ -30,12 +32,21 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        TableLayout table = findViewById(R.id.board);
+        TableLayout table = findViewById(R.id.table);
         for (int i = 0; i < 3; i++) {
             TableRow row = (TableRow) table.getChildAt(i);
             for (int j = 0; j < 3; j++) {
                 Button btn = (Button) row.getChildAt(j);
                 btn.setOnClickListener(new CellListener(i, j));
+            }
+        }
+        if (savedInstanceState != null) {
+            player1Turn = savedInstanceState.getBoolean("turn");
+            byte[] arr = savedInstanceState.getByteArray("board");
+            for (int i = 0; i < 3; i++) {
+                for (int j = 0; j < 3; j++) {
+                    board[i][j] = arr[i * 3 + j];
+                }
             }
         }
 
@@ -52,8 +63,7 @@ public class MainActivity extends AppCompatActivity {
         @Override
         public void onClick(View v) {
             if (board[row][col] != EMPTY_VALUE) {
-                Toast.makeText(MainActivity.this, "Cell is already occupied!",
-                        Toast.LENGTH_SHORT).show();
+                Toast.makeText(MainActivity.this, "Cell is already occupied!", Toast.LENGTH_SHORT).show();
                 return;
             }
             byte playerValue = EMPTY_VALUE;
@@ -67,11 +77,10 @@ public class MainActivity extends AppCompatActivity {
                 playerValue = PLAYER_2_VALUE;
 
             }
-            player1Turn = !player1Turn;
+            player1Turn =! player1Turn;
             int gameState = gameEnded(row, col, playerValue);
             if (gameState > 0) {
-                Toast.makeText(MainActivity.this, "Player " + gameState +
-                        " has won!", Toast.LENGTH_SHORT).show();
+                Toast.makeText(MainActivity.this, "Player " + gameState + " has won!", Toast.LENGTH_SHORT).show();
                 setBoardEnabled(false);
             }
         }
@@ -80,7 +89,7 @@ public class MainActivity extends AppCompatActivity {
 
     public int gameEnded(int row, int col, byte playerValue) {
 
-        //for checking columns
+        //to check columns
         boolean win = true;
         for (int r = 0; r < 3; r++) {
             if (board[r][col] != playerValue) {
@@ -92,7 +101,7 @@ public class MainActivity extends AppCompatActivity {
         if (win)
             return playerValue;
 
-        //for checking rows
+        //to check rows
         win = true;
         for (int c = 0; c < 3; c++) {
             if (board[row][c] != playerValue) {
@@ -104,7 +113,7 @@ public class MainActivity extends AppCompatActivity {
         if (win)
             return playerValue;
 
-        //check diagonals
+        //to check diagonals
         win = true;
         for (int i = 0; i < 3; i++) {
             if (board[i][i] != playerValue) {
@@ -116,6 +125,8 @@ public class MainActivity extends AppCompatActivity {
         if (win)
             return playerValue;
 
+
+        //to check
         win = true;
         for (int i = 0, j = 2; i < 3 && j >= 0; i++, j--) {
             if (board[i][j] != playerValue) {
@@ -131,7 +142,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     void setBoardEnabled(boolean enable) {
-        TableLayout table = findViewById(R.id.board);
+        TableLayout table = findViewById(R.id.table);
         for (int i = 0; i < 3; i++) {
             TableRow row = (TableRow) table.getChildAt(i);
             for (int j = 0; j < 3; j++) {
@@ -162,7 +173,76 @@ public class MainActivity extends AppCompatActivity {
                 btn.setText("");
             }
         }
+
         setBoardEnabled(true);
+        return true;
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState){
+        super.onSaveInstanceState(outState);
+        outState.putByteArray("board",toArray(board));
+        outState.putBoolean("turn",player1Turn);
+    }
+    private byte[] toArray(byte[][] matrix) {
+        int row = matrix.length;
+        int col = matrix[0].length;
+        byte[] arr = new byte[row * col];
+
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                arr[i * 3 + j] = board[i][j];
+            }
+        }
+        return arr;
+
+    }
+    public boolean saveGame(MenuItem item) {
+        SharedPreferences preferences = getPreferences(Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = preferences.edit();
+
+        long b = 0;
+
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                b += (long) (board[i][j] * Math.pow(10, i * 3 + j));
+            }
+        }
+
+        editor.putLong("board", b);
+        editor.putBoolean("turn", player1Turn);
+        editor.commit();
+
+        return true;
+    }
+    public boolean loadGame(MenuItem item) {
+        SharedPreferences preferences = getPreferences(Context.MODE_PRIVATE);
+        player1Turn = preferences.getBoolean("turn", true);
+        long b = preferences.getLong("board", 0);
+
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                board[i][j] = (byte) ((b / Math.pow(10, i * 3 + j)) % 10);
+            }
+        }
+
+        //update the button labels
+        TableLayout table = findViewById(R.id.table);
+        for (int i = 0; i < 3; i++) {
+            TableRow row = (TableRow) table.getChildAt(i);
+            for (int j = 0; j < 3; j++) {
+                Button btn = (Button) row.getChildAt(j);
+                switch (board[i][j]) {
+                    case 0: btn.setText("");
+                        break;
+                    case 1: btn.setText(PLAYER_1_SYMBOL);
+                        break;
+                    case 2: btn.setText(PLAYER_2_SYMBOL);
+                        break;
+                }
+            }
+        }
+
         return true;
     }
 }
